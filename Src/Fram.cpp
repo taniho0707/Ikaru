799f0a6e31279c39eda44b78e904486c31ab04d0
taniho_0707 <taniho_0707@yahoo.co.jp>
2017-09-28 16:47:05 +0900

    Framに書き込めるように

--- Src/Fram.cpp
+++ Src/Fram.cpp
@@ -16,7 +16,6 @@ Fram::Fram() :
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, GPIO_PIN_SET);
 
 	GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5;
 	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -86,10 +85,10 @@ bool Fram::readDeviceId(){
 	// HAL_SPI_TransmitReceive(&port, writedata, readdata, 5, 1000);
 	// resetChipSelect();
 
-	if (readdata[1] == 0x04
-		&& readdata[2] == 0x7F
-		&& readdata[3] == 0x01
-		&& readdata[4] == 0x01) {
+	if (readdata[0] == 0x04
+		&& readdata[1] == 0x7F
+		&& readdata[2] == 0x01
+		&& readdata[3] == 0x01) {
 		return true;
 	} else {
 		return false;
@@ -107,60 +106,72 @@ int Fram::writeEnable(){
 int Fram::writeData(const std::vector<uint8_t> &data, const uint16_t addr, const uint8_t num){
 	std::vector<uint8_t> writedata(num+3);
 	writedata[0] = static_cast<uint8_t>(FramCommands::WRITE);
-	writedata[1] = static_cast<uint8_t>((addr >> 8)&0x00FF);
-	writedata[2] = static_cast<uint8_t>(addr&0x00FF);
+	writedata[1] = static_cast<uint8_t>(((addr) >> 8)&0x00FF);
+	writedata[2] = static_cast<uint8_t>((addr)&0x00FF);
 	for (int i=0; i<num; i++)
 		writedata[i+3] = data[i];
+	writeEnable();
+	HAL_Delay(1);
 	return rwMultiByte(writedata, writedata, 0, num+3);
 }
 
 int Fram::readData(std::vector<uint8_t> &data, const uint16_t addr, const uint8_t num){
 	std::vector<uint8_t> writedata(3);
 	writedata[0] = static_cast<uint8_t>(FramCommands::READ);
-	writedata[1] = static_cast<uint8_t>((addr >> 8)&0x00FF);
-	writedata[2] = static_cast<uint8_t>(addr&0x00FF);
-	return rwMultiByte(data, writedata, num, 3);
+	writedata[1] = static_cast<uint8_t>(((addr) >> 8)&0x00FF);
+	writedata[2] = static_cast<uint8_t>((addr)&0x00FF);
+	rwMultiByte(data, writedata, num, 3);
+	return 0;
 }
 
 bool Fram::saveMap(const Map& map, const int num){
-	uint16_t baseaddr = 1000 + 376 * num; // 1 map is 376 byte
+	uint16_t baseaddr = 376 * num; // 1 map is 376 byte
 	std::vector<uint8_t> v(128);
 	for(int i=0; i<31; ++i){
 		v.at(4*i  ) = (map.column[i] & 0xFF000000) >> 24;
 		v.at(4*i+1) = (map.column[i] & 0x00FF0000) >> 16;
 		v.at(4*i+2) = (map.column[i] & 0x0000FF00) >> 8;
-		v.at(4*i+3) = map.column[i] & 0x000000FF;
+		v.at(4*i+3) = (map.column[i] & 0x000000FF);
 	}
 	writeData(v, baseaddr, 124);
 	for(int i=0; i<31; ++i){
 		v.at(4*i  ) = (map.row[i] & 0xFF000000) >> 24;
 		v.at(4*i+1) = (map.row[i] & 0x00FF0000) >> 16;
 		v.at(4*i+2) = (map.row[i] & 0x0000FF00) >> 8;
-		v.at(4*i+3) = map.row[i] & 0x000000FF;
+		v.at(4*i+3) = (map.row[i] & 0x000000FF);
 	}
 	writeData(v, baseaddr+124, 124);
 	for(int i=0; i<32; ++i){
 		v.at(4*i  ) = (map.reached[i] & 0xFF000000) >> 24;
 		v.at(4*i+1) = (map.reached[i] & 0x00FF0000) >> 16;
 		v.at(4*i+2) = (map.reached[i] & 0x0000FF00) >> 8;
-		v.at(4*i+3) = map.reached[i] & 0x000000FF;
+		v.at(4*i+3) = (map.reached[i] & 0x000000FF);
 	}
 	writeData(v, baseaddr+248, 128);
 	return true;
 }
 
 bool Fram::loadMap(Map& map, const int num){
-	uint16_t baseaddr = 1000 + 376 * num;
+	uint16_t baseaddr = 376 * num;
 	std::vector<uint8_t> v(128);
 	readData(v, baseaddr, 124);
 	for(auto i=0; i<31; ++i)
-		map.column[i] = ((static_cast<uint32_t>(v[4*i]) << 24) | (static_cast<uint32_t>(v[4*i+1]) << 16) | (static_cast<uint32_t>(v[4*i+2]) << 8) | (static_cast<uint32_t>(v[4*i+3])));
+		map.column[i] = ((  static_cast<uint32_t>(v[4*i]) << 24)
+						 | (static_cast<uint32_t>(v[4*i+1]) << 16)
+						 | (static_cast<uint32_t>(v[4*i+2]) << 8)
+						 | (static_cast<uint32_t>(v[4*i+3])));
 	readData(v, baseaddr+124, 124);
 	for(auto i=0; i<31; ++i)
-		map.row[i] = ((static_cast<uint32_t>(v[4*i]) << 24) | (static_cast<uint32_t>(v[4*i+1]) << 16) | (static_cast<uint32_t>(v[4*i+2]) << 8) | (static_cast<uint32_t>(v[4*i+3])));
+		map.row[i] = ((  static_cast<uint32_t>(v[4*i]) << 24)
+					  | (static_cast<uint32_t>(v[4*i+1]) << 16)
+					  | (static_cast<uint32_t>(v[4*i+2]) << 8)
+					  | (static_cast<uint32_t>(v[4*i+3])));
 	readData(v, baseaddr+248, 128);
 	for(auto i=0; i<32; ++i)
-		map.reached[i] = ((static_cast<uint32_t>(v[4*i]) << 24) | (static_cast<uint32_t>(v[4*i+1]) << 16) | (static_cast<uint32_t>(v[4*i+2]) << 8) | (static_cast<uint32_t>(v[4*i+3])));
+		map.reached[i] = ((  static_cast<uint32_t>(v[4*i]) << 24)
+						  | (static_cast<uint32_t>(v[4*i+1]) << 16)
+						  | (static_cast<uint32_t>(v[4*i+2]) << 8)
+						  | (static_cast<uint32_t>(v[4*i+3])));
 	return true;
 }
 
